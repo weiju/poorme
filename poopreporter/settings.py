c655e8fd6ab15ab7d131f46a946f200c4a34b255
Wei-ju Wu <weiju.wu@gmail.com>
2013-01-26 14:15:56 -0800

    added basic design

--- poopreporter/settings.py
+++ poopreporter/settings.py
@@ -56,7 +56,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/static/'
+#STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,8 +68,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #'/home/weiju/StartupWeekend/django/poopreporter/static',
-    '/var/www/dookiedj/static',
+    '/home/weiju/StartupWeekend/django/poopreporter/static',
+    #'/var/www/dookiedj/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -108,8 +109,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #'/home/weiju/StartupWeekend/django/poopreporter/templates',
-    '/var/www/dookiedj/templates',
+    '/home/weiju/StartupWeekend/django/poopreporter/templates',
+    #'/var/www/dookiedj/templates',
 )
 
 INSTALLED_APPS = (
