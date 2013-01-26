020f9f2eb671f11895142ae1a60ab13e9cedfb9f
Wei-ju Wu <weiju.wu@gmail.com>
2013-01-26 15:12:07 -0800

    conditional navigation

--- poopreporter/views.py
+++ poopreporter/views.py
@@ -3,7 +3,9 @@ from django.template import RequestContext
 from django.http import HttpResponse
 
 def index(request):
+    homeclass = 'navlinksel'
     return render_to_response('index.html', locals())
 
 def about(request):
+    aboutclass = 'navlinksel'
     return render_to_response('about.html', locals())
