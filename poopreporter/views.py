e1e1afde9fa65a404775e574db80dee9f9a3c4c5
Wei-ju Wu <weiju.wu@gmail.com>
2013-01-27 09:31:02 -0800

    input form can now be saved

--- poopreporter/views.py
+++ poopreporter/views.py
@@ -4,11 +4,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
 from models import *
 
-class StatusForm(forms.Form):
+class StatusForm(forms.ModelForm):
+    class Meta:
+        model = Status
+        exclude = ['replies', 'status']
+
     name = forms.CharField(max_length=200, required=False)
     anonymous = forms.BooleanField(required=False)
     status = forms.CharField(max_length=1000, required=False)
     zipcode = forms.CharField(max_length=20, required=False)
+    symptoms = forms.TypedMultipleChoiceField(coerce=int, choices=[(s.id, s.name) for s in Symptom.objects.all()])
 
 def index(request):
     homeclass = 'navlinksel'
@@ -30,21 +35,24 @@ def communication(request):
     return render_to_response('communication.html', locals())
 
 def input(request):
-    symptoms = [s.name for s in Symptom.objects.all()]
+    symptoms = Symptom.objects.all()
     if request.method == 'POST':
-        form = StatusForm(request.POST)
+        form = StatusForm(request.POST, symptoms)
         if form.is_valid():
             name = form.cleaned_data['name']
             anonymous = form.cleaned_data['anonymous']
+            if anonymous:
+                name = '(anonymous)'
             status = form.cleaned_data['status']
-            zipcode = form.cleaned_data['zipcode']
-            print "name: ", name
-            print "anonymous: ", anonymous
-            print "status: ", status
-            print "zipcode: ", zipcode
-
+            mystatus = form.save(commit=False)
+            comment = Comment(author=name, text=status)
+            comment.save()
+            mystatus.status = comment
+            mystatus.save()
             return HttpResponseRedirect('/')
+        else:
+            errors = form.errors
     else:
         print "empty form"
-        form = StatusForm()
+        form = StatusForm(symptoms)
     return render(request, 'input.html', locals())
