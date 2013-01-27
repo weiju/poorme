717865f1cafe27c7e88daeb65a2d73f12064e627
Wei-ju Wu <weiju.wu@gmail.com>
2013-01-26 22:05:25 -0800

    example status entry

--- poopreporter/sql/comment.sql
+++ poopreporter/sql/comment.sql
@@ -1,4 +1,4 @@
-insert into poopreporter_comment (id, author, text) values (1, 'John', 'I am feeling terrible :-(');
-insert into poopreporter_comment (id, author, text) values (2, 'Mary', 'Sorry you\'re sick.');
-insert into poopreporter_comment (id, author, text) values (3, 'Jack', 'Feel better.');
+insert into poopreporter_comment (author, text) values ('John', 'I am feeling terrible :-(');
+insert into poopreporter_comment (author, text) values ('Mary', 'Sorry you\'re sick.');
+insert into poopreporter_comment (author, text) values ('Jack', 'Feel better.');
 
---
+++ poopreporter/sql/status.sql
@@ -0,0 +1,1 @@
+insert into poopreporter_status (name, status_id, zipcode) values ('John', 1, '98101');
