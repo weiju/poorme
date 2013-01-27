3e256786dd6296608a281921fdcff1cdac2fd15b
Wei-ju Wu <weiju.wu@gmail.com>
2013-01-27 13:52:02 -0800

    illegal zip queries are not shown anymore

--- poopreporter/sql/stuff.sql
+++ poopreporter/sql/stuff.sql
@@ -3,3 +3,5 @@ insert into poopreporter_stuff (name) values ('flowers');
 insert into poopreporter_stuff (name) values ('cupcakes');
 insert into poopreporter_stuff (name) values ('fruit basket');
 insert into poopreporter_stuff (name) values ('DVD');
+insert into poopreporter_stuff (name) values ('Aleve');
+insert into poopreporter_stuff (name) values ('Tylenol');
