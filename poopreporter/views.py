df449f7acdd4e4796daeb36d545448326cf0c67a
Wei-ju Wu <weiju.wu@gmail.com>
2013-01-26 22:47:42 -0800

    added input form for the status

--- poopreporter/views.py
+++ poopreporter/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
 from models import *
+
+class StatusForm(forms.Form):
+    name = forms.CharField(max_length=200, required=False)
+    anonymous = forms.BooleanField(required=False)
+    status = forms.CharField(max_length=1000, required=False)
+    zipcode = forms.CharField(max_length=20, required=False)
 
 def index(request):
     homeclass = 'navlinksel'
@@ -24,4 +31,20 @@ def communication(request):
 
 def input(request):
     symptoms = [s.name for s in Symptom.objects.all()]
-    return render_to_response('input.html', locals())
+    if request.method == 'POST':
+        form = StatusForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            anonymous = form.cleaned_data['anonymous']
+            status = form.cleaned_data['status']
+            zipcode = form.cleaned_data['zipcode']
+            print "name: ", name
+            print "anonymous: ", anonymous
+            print "status: ", status
+            print "zipcode: ", zipcode
+
+            return HttpResponseRedirect('/')
+    else:
+        print "empty form"
+        form = StatusForm()
+    return render(request, 'input.html', locals())
