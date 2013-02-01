75f0283fcd07dc81c664ec7a3963628765aafb97
Kathleen Tuite <kathleen.tuite@gmail.com>
2013-01-31 20:22:05 -0800

    a few symptoms for john

--- poopreporter/sql/status_symptoms.sql
+++ poopreporter/sql/status_symptoms.sql
@@ -1,1 +1,2 @@
-insert into poopreporter_status_symptoms (status_id, symptom_id) values (1, 1), (1,3), (1, 5);
+insert into poopreporter_status_symptoms (status_id, symptom_id) values (1, 1);
+insert into poopreporter_status_symptoms (status_id, symptom_id) values (1, 3);
