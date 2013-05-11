2aa458d07361fef9d747b7259f1012fde2d9fc4c
Kathleen Tuite <kathleen.tuite@gmail.com>
2013-05-11 16:18:23 -0700

    social media sharing links on main page and different color gradient

--- static/javascripts/clustermap.js
+++ static/javascripts/clustermap.js
@@ -116,7 +116,23 @@
     
         symptoms_point_array = new google.maps.MVCArray(concat_symptoms);
         heatmap = new google.maps.visualization.HeatmapLayer({
-            data: symptoms_point_array
+            data: symptoms_point_array,
+            gradient: [
+                'rgba(0, 255, 255, 0)',
+                'rgba(0, 255, 255, 1)',
+                'rgba(0, 191, 255, 1)',
+                'rgba(0, 127, 255, 1)',
+                'rgba(0, 63, 255, 1)',
+                'rgba(0, 0, 255, 1)',
+                'rgba(0, 0, 223, 1)',
+                'rgba(0, 0, 191, 1)',
+                'rgba(0, 0, 159, 1)',
+                'rgba(0, 0, 127, 1)',
+                'rgba(63, 0, 91, 1)',
+                'rgba(127, 0, 63, 1)',
+                'rgba(191, 0, 31, 1)',
+                'rgba(255, 0, 0, 1)'
+              ]
         });
         heatmap.setMap(map);
     }
